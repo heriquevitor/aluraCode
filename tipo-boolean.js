2532ff92c6ecb1c4fce0e45f611d883a9a09cdfd
heriquevitor3@gmail.com <heriquevitor3@gmail.com>
2024-11-04 21:09:13 -0300

    Aula3

---
+++ tipo-boolean.js
@@ -0,0 +1,9 @@
+const estudante = `Fernando`;
+const estaProvado = true;
+
+
+if(estaProvado === true){
+  console.log(`vai com deus`);
+} else{
+  console.log("REprovou marmota");
+}
---
+++ tipo-string.js
@@ -0,0 +1,16 @@
+// podemos usar uma aspa simples dentro de uma aspas duplas ou vice versa
+
+const cumprimento = `Nossa lema é: "feijao com arroz"`;
+const citacao = `Ju diz: Nosso lema é "Estudar"`;
+
+console.log(`${cumprimento}, ${citacao}`);
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+const tique = '\u2705';
+const hiragana = '\u3041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
