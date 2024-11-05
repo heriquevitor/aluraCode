329852a87d03dd28859f6a0938e1ebb885f884b3
heriquevitor3@gmail.com <heriquevitor3@gmail.com>
2024-11-05 20:35:53 -0300

    Aula 4 Training

--- exercicios.js
+++ exercicios.js
@@ -1,9 +1,9 @@
 //Programação é prática! Criamos mais uma lista de atividades (não obrigatórias) para você exercitar e reforçar ainda mais seu aprendizado. Bora praticar?
 //Declare três variáveis diferentes (uma para cada tipo: string, número e booleano) e atribua valores a elas. Em seguida, exiba o tipo de cada variável no console.
 const respostasDev = "Aqui esta o resultado da questão:\n";
-let primeira = "Eu sou uma stringa";
-let segunda = 10;
-let terceira = true;
+const primeira = "Eu sou uma stringa";
+const segunda = 10;
+const terceira = true;
 console.log(respostasDev);
 console.log(`A primeira opção é uma string ${primeira}, já a segunda opção é um número ${segunda} e a terceira um booleano ${terceira}`);
 console.log(typeof(primeira),typeof(segunda), typeof(terceira));
@@ -11,19 +11,19 @@ console.log(typeof(primeira),typeof(segunda), typeof(terceira));
 
 //Crie duas variáveis, uma contendo seu primeiro nome e outra contendo seu último nome. Em seguida, combine-as em uma terceira variável usando o operador + e em uma quarta variável usando template strings. Por fim, imprima os resultados obtidos no console.
 console.log(respostasDev);
-let nome = "Herique"
-let sobreNome = "Oliveira"
-let nomeSobrenome = nome + sobreNome;
-let diferente = `Nome é ${nome} ${sobreNome}`;  
+const nome = "Herique"
+const sobreNome = "Oliveira"
+const nomeSobrenome = nome + sobreNome;
+const diferente = `Nome é ${nome} ${sobreNome}`;  
 console.log(nomeSobrenome);
 console.log(diferente);
 
 
 //Declare duas variáveis, uma contendo um número e outra contendo uma string. Em seguida, combine-as em uma terceira variável usando template strings para montar uma frase e exiba o resultado no console.
 console.log(respostasDev);
-let temNumero = 22;
-let temString = "Minha idade";
-let combinaVariavel = `Essa é a variavél do número, contendo ${temNumero}, e essa é a variavel da string contendo ${temString}`;
+const temNumero = 22;
+const temString = "Minha idade";
+const combinaVariavel = `Essa é a variavél do número, contendo ${temNumero}, e essa é a variavel da string contendo ${temString}`;
 console.log(combinaVariavel);
 
 
@@ -39,10 +39,71 @@ console.log(variavelVariada, variavelVariada)
 //Declare uma variável booleana que informa se está chovendo e utilize-a em uma estrutura condicional (if) para exibir uma mensagem informando se é preciso levar um guarda-chuva ou não dependendo do valor da variável.
 
 console.log(respostasDev);
-let choveChuva = true;
+const choveChuva = true;
 if(choveChuva === true){
   console.log("Levar guarda-chuva pois vai chover")
 } else{
   console.log("Não precisa levar o guarda-chuva");
 }
 console.log(choveChuva);
+
+
+//Crie uma variável contendo uma frase. Em seguida, exiba no console o comprimento da frase e a mesma frase com letras maiúsculas.
+
+const temFrase = `Aqui tem uma frase`;
+const comprimentoSemEspaco = temFrase.replace(/\s/g,"").length;
+console.log(`comprimento com espaços em branco é: ${temFrase.length}`);
+console.log(`comprimento sem espaços em branco é: ${comprimentoSemEspaco}`);
+console.log(temFrase.toLocaleUpperCase());
+
+
+//Declare duas variáveis, uma inicializada com valor null e outra que não recebe atribuição (resultando em undefined). Exiba os valores no console.
+
+const variandoNu = null;
+let variandoUnd;
+console.log(variandoNu);
+console.log(variandoUnd);
+console.log(typeof variandoUnd)
+if(variandoNu === null){
+  console.log(`a variavel ${variandoNu} é null`);
+}
+
+//Crie 3 variáveis de tipos diferentes, utilize template strings para combinar os diferentes tipos de dados (number, string, boolean) em uma única string e exiba o resultado no console.
+
+const papelAzul= `Aqui é azul`;
+
+const papelAmarelo = 5.55
+
+const papelPreto= true;
+
+let papelRosa;
+
+console.log(typeof papelAzul,papelAmarelo,papelPreto,papelRosa);
+console.log(`O papel azul é ${papelAzul} do tipo: ${typeof papelAzul}`);
+console.log(`O papel amarelo é ${papelAmarelo} do tipo: ${typeof papelAmarelo}`);
+console.log(`O papel preto é ${papelPreto} do tipo: ${typeof papelPreto}`);
+console.log(`O papel rosa é ${papelRosa} do tipo: ${typeof papelRosa}`);
+
+//Crie uma variável numérica e uma string. Faça a conversão da variável numérica para string e da string para número. Exiba os tipos de dados resultantes no console.
+
+let numeroAqui = 1;
+
+let fraseAqui = `2`;
+
+let numeroConvertido = String(numeroAqui);
+
+let fraseConvertido = Number(fraseAqui);
+let fraseConvertido2 = parseInt(fraseAqui);
+
+console.log(numeroConvertido);
+console.log(fraseConvertido);
+console.log(fraseConvertido2);
+
+//Crie uma variável com uma string e utilize métodos de manipulação de strings, como toUpperCase, toLowerCase, slice ou outros, para modificar a string original. Exiba os resultados finais no console.
+
+let brincaString = `Minha cachorra é linda`;
+
+console.log(brincaString.toLocaleUpperCase());
+console.log(brincaString.toLocaleLowerCase());
+console.log(brincaString.slice(6,14));
+console.log(brincaString.substring(1,5));
---
+++ tipo-null-undefined.js
@@ -0,0 +1,10 @@
+let nomeEstudante;
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+
+//null x undefined pq null a gente atribui(situações de cadastro q campo n tem valor etc)
+//NaN ele é um valor do tipo numero que representa operação matematica má formada
+
+
+
+console.log(Number(true));
