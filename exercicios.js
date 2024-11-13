baf98f0a94a0b2e2cb310282e406f22743d4b1e3
heriquevitor3@gmail.com <heriquevitor3@gmail.com>
2024-11-13 18:34:54 -0300

    Aula 7 arrays e exercicio

--- exercicios.js
+++ exercicios.js
@@ -389,3 +389,78 @@ clinica.pop();
 clinica.pop();
 clinica.pop();
 console.log(clinica);
+
+
+//Dominar o uso de arrays é fundamental em programação e uma base essencial na manipulação de dados. Praticar regularmente é a chave para se tornar mais confiante!
+
+//Para isso criamos uma lista de exercícios que não apenas ajudam a explorar os métodos de manipulação de arrays, mas também vão te preparar para lidar eficientemente com dados em aplicações reais.
+
+//1 - Crie uma função que receba dois arrays e os concatene em um único array.
+
+
+function concatenateArrays(arr1, arr2) {
+  return arr1.concat(arr2); // Concatena os dois arrays
+}
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const concatenatedArray = concatenateArrays(array1, array2);
+console.log(concatenatedArray); // Saída: [1, 2, 3, 4, 5, 6]
+
+//2 - Crie um array chamado numeros contendo números de 1 a 10. Utilize o método slice para criar um novo array chamado parteNumeros que contenha apenas os números de índice 3 a 7 de numeros.
+
+const numerosLindos = [1,2,3,4,5,6,7,8,9,10];
+
+const parteNumeros = numerosLindos.slice(3,8);
+console.log(parteNumeros);
+
+
+//3 - Dado o array frutas contendo frutas que desejamos comprar na feira:
+//Utilize o método splice para remover as frutas no índice 2 e 3 e, em seguida, adicione as frutas 'Kiwi' e 'Pêssego' nesses mesmos índices.
+
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi']
+
+frutas.splice(2,2,`Kiwi`,`Pêssego`);
+console.log(frutas);
+
+
+
+//4 - Crie dois arrays chamados menuPrincipal e menuDeSobremesas contendo opções do cardápio de um restaurante. Utilize o método concat para criar um novo array menuCompleto contendo todos os elementos de menuPrincipal seguidos pelos elementos de menuDeSobremesas.
+
+const menuPrincipal = [`Batata-Frita`,`Arroz`,`Ovo`];
+const menuDeSobremesas = [`Brigadeiro`,`Maçã do Amor`];
+
+const menuCompleto = menuPrincipal.concat(menuDeSobremesas);
+console.log(menuCompleto);
+
+
+
+//5 - Crie uma lista bidimensional com 3 linhas e 3 colunas, onde cada elemento seja uma matriz 3x3 com valores iniciando em 1 e aumentando em 1 a cada elemento.
+//comece com um array vazio, por exemplo const matriz = [] e adicione valores nele com push;
+//você pode resolver usando um for dentro de outro for.
+const listaDimensionalVazia = [];
+let contador = 1;
+
+for(let i = 0; i < 3; i++){
+  const linha = [];
+  for(let j = 0; j < 3; j++){
+    linha.push(contador);
+  contador++
+}
+listaDimensionalVazia.push(linha);
+}
+
+
+
+console.log(listaDimensionalVazia);
+
+
+//6 - Acesse e imprima o elemento na segunda linha e terceira coluna da lista bidimensional matriz criada no exercício anterior.
+
+console.log(listaDimensionalVazia[1][2]);
+
+
+//7 - Adicione um novo elemento (por exemplo,15) na terceira linha e segunda coluna da lista bidimensional matriz criada anteriormente.
+
+listaDimensionalVazia[2][1] = 15;
+console.log('Matriz após adição de elemento:', listaDimensionalVazia);
